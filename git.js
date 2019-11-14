64e7f24c6ddb79ae471c4f498f7d74b299056664
The-Nico26 <7610443+The-Nico26@users.noreply.github.com>
2019-11-14 15:24:21 +0100

    Test git deploy

--- git.js
+++ git.js
@@ -1,19 +1,18 @@
-var config = require('../config'),
-    git = require('simple-git')(__dirname+'/..'),
+var config = require('./config'),
+    git = require('simple-git')(__dirname),
     net = require('net'),
     JsonSocket = require('json-socket');
-var dns = require('dns')
+
 var socket = new JsonSocket(new net.Socket());
 socket.connect(config.port_webhook, config.server_webhook);
 
-git.fetch();
 git.checkout(config.branch);
 
 socket.on('connect', ()=>{
     socket.sendMessage({'secret':config.secret, 'type':'init', 'git':config.git});
     socket.on('message', (html)=>{
-        let t = git.fetch();
-        console.log(git.checkIsRepo());
+        console.log(html);
+        git.pull();
     })
     socket.on('close', (message)=>{
         console.log(message);
